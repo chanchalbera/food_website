0f8374fe1422e3f14189b2a22c04a41e4b666434
Chanchal Bera <chanchalbera1863@gmail.com>
2023-02-02 22:54:05 +0530

    add media query

---
+++ darkmode.js
@@ -0,0 +1,18 @@
+function darkMode() {
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    console.log('dark ');
+    
+    element.className = "dark-mode";
+    content.innerText = "Dark Mode is ON";
+    
+    
+}
+
+
+function lightMode() {
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    element.className = "light-mode";
+    content.innerText = "Dark Mode is OFF";
+}
